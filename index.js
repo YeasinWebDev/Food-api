6ef4f5c8ab0e0314b5e8e74e2210c1cb842e553e
Yeasin <ar9485727@gmail.com>
2024-08-20 22:43:11 +0600

    server problem

--- index.js
+++ index.js
@@ -370,7 +370,7 @@ async function run() {
   
   
 
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
   } finally {
